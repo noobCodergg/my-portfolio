597ed555d1070ef28a141fb3b05e81b32f61d553
noobCodergg <113461375+noobCodergg@users.noreply.github.com>
2025-04-20 18:48:36 +0600

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
+const connectDB = require("./Config/db");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser"); // Import cookie-parser
 const prizeRoute = require("./Routes/prizeRoute");
